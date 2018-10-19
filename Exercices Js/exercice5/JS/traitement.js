e8602e416b442f586ead3fa9ffb31f51e15f684b
plono1830662 <plono1830662@etu.cegepjoquiere.ca>
2018-10-19 11:03:38 -0400

    exercice 5 Java

--- Exercices Js/exercice5/JS/traitement.js
+++ Exercices Js/exercice5/JS/traitement.js
@@ -1,5 +1,14 @@
 function btnCalculer_onclick()
-{ 
- 	
+{
+    var Rev, Loy, Nour, Dep, Res;
+
+    Rev=parseFloat(document.getElementById("txtRev").value);
+    Loy=parseFloat(document.getElementById("txtLoy").value);
+    Nour=parseFloat(document.getElementById("txtNour").value);
+    Dep=parseFloat(document.getElementById("txtDep").value);
+
+    Res=(Rev-Loy-Nour-Dep)/4.33;
+
+    console.log( "Il vous restera "+Res.toFixed(2)+ " $ par semaine")
 
 }
