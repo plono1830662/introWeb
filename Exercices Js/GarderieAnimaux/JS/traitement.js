691ef298ac576e6e2fb3817ea14128229697ede7
plono1830662 <plono1830662@etu.cegepjoquiere.ca>
2018-10-24 10:58:59 -0400

    exercice garderie Java

---
+++ Exercices Js/GarderieAnimaux/JS/traitement.js
@@ -0,0 +1,20 @@
+function btnCalculer_onclick()
+{ 
+ 	var Veterinaire,NbreJours,Serv,Prix,Message,CoutTot;
+
+    Veterinaire=document.getElementById("lstVeterinaire");
+    NbreJours=parseInt(document.getElementById("txtNbreJours").value);
+
+    if(document.getElementById("chkServ").checked==true)
+    {
+            Serv=5;
+    }
+    else {
+        Serv=0;
+    }
+
+
+
+
+
+}
