0bdcdbb44bfdb70cea4b647af2d07313d4fb80cb
plono1830662 <plono1830662@etu.cegepjoquiere.ca>
2018-10-25 17:42:36 -0400

    exercice garderie Java presque fini

--- Exercices Js/GarderieAnimaux/JS/traitement.js
+++ Exercices Js/GarderieAnimaux/JS/traitement.js
@@ -1,20 +1,32 @@
 function btnCalculer_onclick()
 { 
- 	var Veterinaire,NbreJours,Serv,Prix,Message,CoutTot;
+ 	var Veterinaire,NbreJours,Serv,Prix,Message,CoutTot, CoutTotTaxe;
 
     Veterinaire=document.getElementById("lstVeterinaire");
     NbreJours=parseInt(document.getElementById("txtNbreJours").value);
 
     if(document.getElementById("chkServ").checked==true)
     {
-            Serv=5;
+        Serv=5;
     }
     else {
         Serv=0;
     }
 
+    if(document.getElementById("radChien").checked==true)
+    {
+       Prix= 16.0;
+       Message="chien";
+    }
+    else{
+        Prix=17.0;
+        Message="chat";
+    }
 
+    CoutTot=(Prix*NbreJours)+Serv;
+    CoutTotTaxe=CoutTot+(CoutTot*0.14975);
 
+    console.log("Vétérinaire responsable est"+Veterinaire+". Le prix de la garde de votre "+Message+" est de "+CoutTotTaxe.toFixed(2)+"$ pour "+NbreJours+" jours");
 
 
 }
