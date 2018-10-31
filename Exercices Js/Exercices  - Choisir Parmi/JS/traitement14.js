07f2fd13c5c67ad88d540095f43e5db85452870d
plono1830662 <plono1830662@etu.cegepjoquiere.ca>
2018-10-31 11:00:21 -0400

    exercice 14 fini

--- Exercices Js/Exercices  - Choisir Parmi/JS/traitement14.js
+++ Exercices Js/Exercices  - Choisir Parmi/JS/traitement14.js
@@ -1,5 +1,27 @@
 function btnCalculer_onclick()
 { 
- 	
+ 	var Nbre1,Nbre2,Operateur,Resultat;
+
+ 	Nbre1=parseInt(document.getElementById("txtNbre1").value);
+ 	Nbre2=parseInt(document.getElementById("txtNbre2").value);
+ 	Operateur=document.getElementById("txtOperateur").value;
+
+ 	switch(Operateur)
+    {
+        case "+":
+            Resultat=Nbre1+Nbre2;
+            break;
+        case "-":
+            Resultat=Nbre1-Nbre2;
+            break;
+        case "*":
+            Resultat=Nbre1*Nbre2;
+            break;
+        case"/":
+            Resultat=Nbre1/Nbre2;
+            break;
+    }
+
+    document.getElementById("lblMessage").innerHTML=Resultat;
 
 }
