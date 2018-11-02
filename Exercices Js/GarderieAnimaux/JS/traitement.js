aa1564d8fb7ec10936c8470d7287059e29c4ae4b
plono1830662 <plono1830662@etu.cegepjoquiere.ca>
2018-11-02 10:52:06 -0400

    exercice garderie fini

--- Exercices Js/GarderieAnimaux/JS/traitement.js
+++ Exercices Js/GarderieAnimaux/JS/traitement.js
@@ -1,9 +1,48 @@
 function btnCalculer_onclick()
 { 
- 	var Veterinaire,NbreJours,Serv,Prix,Message,CoutTot, CoutTotTaxe;
+ 	var Veterinaire,NbreJours,Serv,Prix,Message,CoutTot, CoutTotTaxe, Rabais,Salaire,NbreHeure;
 
     Veterinaire = document.getElementById("lstVeterinaire").value;
     NbreJours=parseInt(document.getElementById("txtNbreJours").value);
+    NbreHeure=parseInt(document.getElementById("txtNbreHeure").value);
+
+    if(NbreJours==1&&NbreJours<=5)
+    {
+        Rabais=0;
+    }
+    else {
+        if(NbreJours>5&&NbreJours<=10)
+        {
+            Rabais=0.05; 
+        }
+        else{
+            if (NbreJours>10&&NbreJours<=30)
+            {
+                Rabais=0.1;
+            }
+            else{
+                Rabais=0.15;
+            }
+        }
+        
+    }
+
+    switch(Veterinaire)
+    {
+        case "Audrey Bouchard":
+            Salaire=NbreHeure*25;
+            break;
+        case "Stéphane Tremblay":
+            Salaire=NbreHeure*35;
+            break;
+        case "Maxime Simard":
+            Salaire=NbreHeure*40;
+            break;
+        case "Mélissa Caron":
+            Salaire=NbreHeure*45;
+            break;
+    }
+
 
     if(document.getElementById("chkServ").checked==true)
     {
@@ -23,10 +62,20 @@ function btnCalculer_onclick()
         Message="chat";
     }
 
-    CoutTot=(Prix*NbreJours)+Serv;
-    CoutTotTaxe=CoutTot+(CoutTot*0.14975);
+    CoutTot=(Prix*NbreJours)+Serv+Salaire;
+    CoutTotTaxe=(CoutTot-(CoutTot*Rabais))+(CoutTot*0.14975);
 
-    console.log("Vétérinaire responsable est "+Veterinaire+". Le prix de la garde de votre "+Message+" est de "+CoutTotTaxe.toFixed(2)+"$ pour "+NbreJours+" jours");
+    document.getElementById("lblMessage").innerHTML="Vétérinaire responsable est "+Veterinaire+". Le prix de la garde de votre "+Message+" est de "+CoutTotTaxe.toFixed(2)+"$ pour "+NbreJours+" jours.";
 
 
+}
+function btnAfficher1_onclick()
+{
+    document.clear();
+    document.getElementById("imgChien").src="img/Chien.png";
+}
+function btnAfficher2_onclick()
+{
+    document.clear();
+    document.getElementById("imgChat").src="img/Chat.png";
 }
