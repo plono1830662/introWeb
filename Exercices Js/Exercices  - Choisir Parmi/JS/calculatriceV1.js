a7f758d023907de40ac32016055b0b03b129f496
plono1830662 <plono1830662@etu.cegepjoquiere.ca>
2018-11-14 11:12:28 -0500

    exercice lab calcul

---
+++ Exercices Js/Exercices  - Choisir Parmi/JS/calculatriceV1.js
@@ -0,0 +1,70 @@
+function btnCalculer_onclick()
+{
+
+    var Nbre1,Nbre2,Operateur;
+
+    if (valideChampsObligatoires()===true)
+    {
+        Saisir();
+    }
+
+}
+function Calculer(Nbr1,Nbr2,Operateur)
+{
+
+    var Resultat=0;
+
+    switch(Operateur)
+    {
+        case "+":
+            Resultat=Nbr1+Nbr2;
+            break;
+        case "-":
+            Resultat=Nbr1-Nbr2;
+            break;
+        case "*":
+            Resultat=Nbr1*Nbr2;
+            break;
+        case"/":
+            Resultat=Nbr1/Nbr2;
+            break;
+    }
+    return Resultat;
+}
+function Saisir()
+{
+    var Nbre1,Nbre2,Operateur,Resultat;
+
+    Nbre1=parseInt(document.getElementById("txtNbre1").value);
+    Nbre2=parseInt(document.getElementById("txtNbre2").value);
+    Operateur=document.getElementById("txtOperateur").value;
+    Resultat=Calculer(Nbre1,Nbre2,Operateur);
+    document.getElementById("lblMessage").innerHTML=Resultat;
+
+}
+function valideChampsObligatoires()
+{
+    var Valide=false;
+    if(valideExiste("txtNbre1")===true)
+    {
+        Valide=true;
+    }
+    else {
+        Valide=false;
+    }
+    return Valide;
+}
+function valideExiste(Case)
+{
+    var Valide=false;
+    if(document.getElementById(Case).value==="")
+    {
+        Valide=false;
+        document.getElementById(Case).style.backgroundColor="red";
+    }
+    else{
+        Valide=true;
+        document.getElementById(Case).style.backgroundColor="white";
+    }
+    return Valide;
+}
